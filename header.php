7d973358b508f320eaa9fc467b559d306603476b
stiellerr <reece.stieller@gmail.com>
2021-03-22 07:34:04 +1300

    updates

--- header.php
+++ header.php
@@ -8,11 +8,12 @@
  *
  * @package zthemename
  */
+
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo("charset"); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     
