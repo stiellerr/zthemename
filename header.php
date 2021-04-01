d84a321fcb8101908b4b50cc6aaab36104690754
stiellerr <reece@stieller.com>
2021-04-02 08:50:25 +1300

    updates

--- header.php
+++ header.php
@@ -63,15 +63,19 @@
 		</div>
 	</nav><i class="fas fa-phone-alt" data-content="f879">&#8203;</i>
 	-->
-	<nav class="navbar navbar-expand-md bg-dark <?php echo get_theme_mod( 'nav_theme', 'navbar-light' ); ?>">
+	<nav class="navbar navbar-expand-md <?php echo get_theme_mod( 'nav_theme', 'navbar-light' ); ?>">
 		<div class="container">
-			<?php the_custom_logo(); ?>
-			<div class="wp-block-button order-md-last">
-				<a class="wp-block-button__link" href="tel:0275457737" style="border-radius:4px;"><i class="fas fa-phone-alt" data-content="f879">&nbsp;</i>027 545 7737</a>
-			</div>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+			<div style="flex: 1;">
+				<?php the_custom_logo(); ?>
+</div>
+				<div class="wp-block-button order-md-last">
+					<a class="wp-block-button__link" href="tel:0275457737" style="border-radius:4px;"><i class="fas fa-phone-alt" data-content="f879">&nbsp;</i>027 545 7737</a>
+				</div>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			
+
 			<?php
 			wp_nav_menu(
 				array(
@@ -93,7 +97,7 @@
 				)
 			);
 			?>
-		</div>
+			</div>
 	</nav>
 
 </header>
