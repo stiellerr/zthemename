d6fe0718f1720b340340491fcd1f2cbf5ee0bb42
stiellerr <reece.stieller@gmail.com>
2021-04-11 21:32:03 +1200

    updates

--- src/admin.js
+++ src/admin.js
@@ -1,3 +1,20 @@
 import "./admin.scss";
 
 console.log("admin loaded");
+
+/**
+ * External dependencies
+ */
+import $ from "jquery";
+
+$(document).on("ready", () => {
+    console.log("page ready...");
+
+    $(document).on("change", ".zzz_test", ({ currentTarget }) => {
+        console.log($(currentTarget).val());
+        //console.log(e);
+        // do something
+        console.log("Hello World!");
+    });
+    //var zzz = $(".zzz_test");
+});
