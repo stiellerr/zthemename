c179544ab3067388de27983af6eb1ec0f55ca351
stiellerr <reece@stieller.com>
2021-06-24 16:00:49 +1200

    updates

--- footer.php
+++ footer.php
@@ -9,27 +9,33 @@
  * @package zthemename
  */
 
-?>	
+?>
 
 <footer class="site-footer mt-auto">
 	<div class="container <?php echo esc_html( get_theme_mod( 'nav_theme', 'navbar-light' ) ); ?>">
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
 			<div class="wp-block-columns">
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+				<div class="footer-nav mt-3">
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location'  => 'menu-2',
+								'depth'           => 1,
+								'fallback_cb'     => false,
+								'menu_class'      => 'nav',
+							)
+						);
+					?>
+					<div>
+						<?php
+						/* translators: 1: Theme name, 2: Current year. */
+						printf( esc_html__( 'Powered by %1$s © %2$s.', 'zthemename' ), '<a href="https://zthemename.com/">zthemename</a>', esc_attr( date( 'Y' ) ) );
+						?>
+					</div>
+				</div>
 			</div>
 		<?php } ?>
-		<?php  
-		wp_nav_menu(
-			array(
-				'theme_location' => 'footer',
-				'depth'          => 1,
-				'fallback_cb'    => false,
-				'menu_class'     => 'nav',
-				'menu_id'        => 'menu-footer',
-			)
-		); 
-		?>
-		<div>hello world</div>
 	</div>
 </footer>
 
